38d4c663f5f32a2d8a6b64db53212068d00eef74
Sanganwar Sai Charan <122984649+itsmessc@users.noreply.github.com>
2024-02-01 18:33:05 +0530

    Update 1client.c

--- lab 4/1client.c
+++ lab 4/1client.c
@@ -1,60 +1,67 @@
-#include<stdio.h>
-#include<string.h>
-#include<stdlib.h>
-#include<sys/types.h>
-#include<netinet/in.h>
-#include<sys/socket.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <netinet/in.h>
+#include <sys/socket.h>
 
-int main()
-{
-	int sockfd = socket(AF_INET, SOCK_STREAM, 0);
-	if(sockfd == -1)
-	{	printf("socket creation error");
-		exit(0);
-	}
-	struct sockaddr_in c_addr;
-	c_addr.sin_port = htons(1342);
-	c_addr.sin_family = AF_INET;
-	c_addr.sin_addr.s_addr = inet_addr("127.0.0.1");
-	memset(c_addr.sin_zero, '\0', sizeof(c_addr.sin_zero));
-	
-	int size = sizeof(struct sockaddr);
-	connect(sockfd, (struct sockaddr*) &c_addr, sizeof(c_addr));
+int main() {
+    int sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    if (sockfd == -1) {
+        printf("socket creation error");
+        exit(1);
+    }
 
-	char buff[50];
-	printf("Enter file name:");
-	scanf("%s",&buff);
-	if(send(sockfd,buff,sizeof(buff),0)<0){
-		printf("send failed");
-	}
-	recv(sockfd,buff,sizeof(buff),0);
-	printf("%s\n",buff);
-	int n,count,m;
-	while(1){
-		printf("1.Search\n2.Replace\n3.Rearrange\n4.Exit\n");
-		printf("Enter input:");
-		scanf("%d",&n);
-		switch(n){
-			case 1:
-				// int count;
-				printf("Enter word to search:");
-				scanf("%s",&buff);
-				m=1;
-				if(send(sockfd,&m,sizeof(buff),0)<0){
-					printf("send failed");
-				}
-				if(send(sockfd,buff,sizeof(buff),0)<0){
-					printf("send failed");
-				}
-				if(recv(sockfd,&count,sizeof(count),0)<0){
-					perror("Receving failed");
-				}
-				printf("Repeated %d times",count);
-				break;
+    struct sockaddr_in c_addr;
+    c_addr.sin_port = htons(1342);
+    c_addr.sin_family = AF_INET;
+    c_addr.sin_addr.s_addr = inet_addr("127.0.0.1");
 
-		}
-	}
-	
-	close(sockfd);
-	return 0;
+    int size = sizeof(struct sockaddr);
+    connect(sockfd, (struct sockaddr*) &c_addr, sizeof(c_addr));
+
+    char buff[50];
+    printf("Enter file name:");
+    scanf("%s", buff);
+    if (send(sockfd, buff, sizeof(buff), 0) < 0) {
+        printf("send failed");
+        exit(1);
+    }
+    recv(sockfd, buff, sizeof(buff), 0);
+    printf("%s\n", buff);
+
+    int n, count, m;
+    while (1) {
+        printf("1.Search\n2.Replace\n3.Rearrange\n4.Exit\n");
+        printf("Enter input:");
+        scanf("%d", &n);
+        switch (n) {
+            case 1:
+                printf("Enter word to search:");
+                scanf("%s", buff);
+                m = 1;
+                if (send(sockfd, &m, sizeof(m), 0) < 0) {
+                    printf("send failed");
+                    exit(1);
+                }
+                if (send(sockfd, buff, sizeof(buff), 0) < 0) {
+                    printf("send failed");
+                    exit(1);
+                }
+                if (recv(sockfd, &count, sizeof(count), 0) < 0) {
+                    perror("Receiving failed");
+                    exit(1);
+                }
+                printf("Repeated %d times\n", count);
+                break;
+            case 4:
+                // Add code to send the exit option to the server and then close the connection
+                // ...
+                close(sockfd);
+                exit(0);
+        }
+    }
+
+    close(sockfd);
+    return 0;
 }
